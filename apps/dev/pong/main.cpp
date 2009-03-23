75769774d1566b5a09a26c3e8a68f5da8fce5ca7
Peter Krenn <peter.krenn@gmail.com>
2009-03-24 00:23:03 +0100

    Changed initial window size

--- apps/dev/pong/main.cpp
+++ apps/dev/pong/main.cpp
@@ -13,7 +13,7 @@
 int main(int argc, char *argv[]) {
 	// can be OF_WINDOW or OF_FULLSCREEN
 	// pass in width and height too:
-	ofSetupOpenGL(1024,768, OF_WINDOW);			// <-------- setup the GL context
+	ofSetupOpenGL(800, 600, OF_WINDOW);			// <-------- setup the GL context
 
 	// this kicks off the running of my app
 	ofRunApp(new pongApp);
